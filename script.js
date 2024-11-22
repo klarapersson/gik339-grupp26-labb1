a12df635ea764b4828ca5f5cbf1298bd2a28366f
klarapersson <klara04persson@gmail.com>
2024-11-22 13:29:49 +0100

    hej

--- script.js
+++ script.js
@@ -10,12 +10,17 @@ console.log(buttonStyleElement);
 const emptyDivElement = document.querySelectorAll(".empty_div");
 console.log(emptyDivElement);
 
-
 function handleInputEvent(e) {
-    console.log("Event target:", e.target);
+  console.log("Event target:", e);
 }
-function handleInputEvent(e){
-    const target = e.target;
-    console.log("test", target)
-}
+function handleInputEvent(e) {
+  const target = e.target;
+  console.log("test", target);
+  const targetName = e.target.name;
 
+  if (targetName === "content") {
+    const inputValue = e.target.value;
+    const outputDiv = document.getElementById("outputDiv");
+    outputDiv.innerHTML = inputValue;
+  }
+}
